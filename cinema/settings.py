a58342f5feadaa9ae78f02c769bed92ab8bb396f
Sasun <sasun.31@mail.ru>
2018-04-02 11:26:25 +0300

    cinema

--- cinema/settings.py
+++ cinema/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,11 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiels')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # from django database in heroku
 db_from_env = dj_database_url.config()
