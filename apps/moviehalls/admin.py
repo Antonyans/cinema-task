693db767f7c95031bb9bd9392fa4a974a3daea99
Sasun <sasun.31@mail.ru>
2018-04-02 10:04:59 +0300

    cinema

--- apps/moviehalls/admin.py
+++ apps/moviehalls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.moviehalls.models import RedHall, BlueHall, BlackHall, Movies, Reserved
+from apps.moviehalls.models import RedHall, BlueHall, BlackHall, Halls, Movies, Reserved
 
 
 class HallsAdmin(admin.ModelAdmin):
@@ -9,9 +9,9 @@ class HallsAdmin(admin.ModelAdmin):
         fieldsets = ('movies_red_hall', 'movies_showing')
         model = RedHall
 
-
 admin.site.register(RedHall, HallsAdmin)
 admin.site.register(BlueHall)
 admin.site.register(BlackHall)
+admin.site.register(Halls)
 admin.site.register(Movies)
 admin.site.register(Reserved)
